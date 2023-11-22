3c52d0b8a169d36b7d129ee741c39cf0a92c5f9e
root <snowburn7>
2023-11-22 06:46:51 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a new line. 
+ * main - prints "Programming is like building a multilingual puzzle,"
+ * followed by a new line 
  * Return: Always 0
  */
 
 int main(void)
 
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle,");
 	return (0);
 
 } 
